2b0341d555b91c9d0b347d28ecdbc8f460b5b605
Александр <elsakov982@inbox.ru>
2020-04-24 17:06:51 +0300

    atomicBoolean

--- src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
+++ src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
@@ -1,22 +1,23 @@
 package ru.spsuace.projects.pass_fail.map;
 
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Реализовать контейнер для мапы. Сам класс ClosableMap считается потокобезопасным и с ним проблем нет. Его НЕ трогать.
  * Суть в том, что после вызова метода map.close() любой другой метод map будет кидать ошибкуи крашить данные.
  * Надо избежать этого.
- *
+ * <p>
  * Можно создавать любые дополнительные методы и поля.
  * Так же можно использовать любые механизмы и объекты синхранизации в java.
- *
+ * <p>
  * Считаем, что метод close вызывается только один раз.
  * Все методы могут вызываться из разных потоков
  */
 public class ContainerMap {
 
-    public static AtomicReference<ContainerMap> instanceMap = new AtomicReference<>();
+    private final AtomicBoolean closeOn = new AtomicBoolean(false);
+
 
     private ClosableMap map = new ClosableMap();
 
@@ -24,7 +25,11 @@ public class ContainerMap {
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long put(Long key, Long value) {
-        return map.put(key, value);
+        if (closeOn.get() == false) {
+            return map.put(key, value);
+        } else {
+            return null;
+        }
     }
 
 
@@ -32,7 +37,11 @@ public class ContainerMap {
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long get(Long key) {
-        return map.get(key);
+        if (closeOn.get() == false) {
+            return map.get(key);
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -40,5 +49,6 @@ public class ContainerMap {
      */
     public void close() {
         map.close();
+        closeOn.compareAndSet(false, true);
     }
 }
