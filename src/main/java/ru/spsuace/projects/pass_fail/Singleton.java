f696386dbb94bec920fc040ecf984cbff89c3b2b
AnneTse <tsetselskaya@gmail.com>
2020-04-24 18:48:02 +0300

    Зачет Singleton

--- src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
+++ src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
@@ -1,6 +1,8 @@
 package ru.spsuace.projects.pass_fail;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Надо реализовать ленивый синглтон. Важно, что бы объект можно было получить только через метод getInstance()
  * Так же важно, чтобы объект был создан только один раз и метод getInstance() всегда возвращал один и тот же объект.
@@ -9,7 +11,26 @@ package ru.spsuace.projects.pass_fail;
  */
 public class Singleton {
 
+    private static volatile Singleton INSTANCE;
+    private final int id;
+    private  static final AtomicInteger counter = new AtomicInteger(0);
+
+    public Singleton(int id) {
+       this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
     public static Singleton getInstance() {
-        return null;
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new Singleton(0);
+                }
+            }
+        }
+        counter.getAndIncrement();
+        return INSTANCE;
     }
 }
