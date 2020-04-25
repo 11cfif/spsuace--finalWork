351dd79904582e8b8dc8e3410a4854e9f6af7a84
HOME TASK 1 <lilpowerfulann@gmail.com>
2020-04-25 14:37:37 +0300

    fix 2.0

--- src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
+++ src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
@@ -15,6 +15,7 @@ public class Singleton {
 
     private static volatile Singleton instance;
 
+    private Singleton() {   }
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
--- src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
+++ src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
@@ -1,21 +1,22 @@
 package ru.spsuace.projects.pass_fail.map;
 
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Реализовать контейнер для мапы. Сам класс ClosableMap считается потокобезопасным и с ним проблем нет. Его НЕ трогать.
  * Суть в том, что после вызова метода map.close() любой другой метод map будет кидать ошибкуи крашить данные.
  * Надо избежать этого.
- *
+ * <p>
  * Можно создавать любые дополнительные методы и поля.
  * Так же можно использовать любые механизмы и объекты синхранизации в java.
- *
+ * <p>
  * Считаем, что метод close вызывается только один раз.
  * Все методы могут вызываться из разных потоков
  */
 public class ContainerMap {
-
+    Semaphore semaphore = new Semaphore(1);
     private final AtomicBoolean closeCheck = new AtomicBoolean(false);
     private ClosableMap map = new ClosableMap();
 
@@ -23,9 +24,9 @@ public class ContainerMap {
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long put(Long key, Long value) {
-        synchronized (ContainerMap.class) {
-            if (!closeCheck.get()) {
-                return map.put(key, value);
+        if (!closeCheck.get()) {
+            synchronized (closeCheck) {
+                if (!closeCheck.get()) return map.put(key, value);
             }
         }
         return null;
@@ -36,9 +37,9 @@ public class ContainerMap {
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long get(Long key) {
-        synchronized (ContainerMap.class) {
-            if (!closeCheck.get()) {
-                return map.get(key);
+        if (!closeCheck.get()) {
+            synchronized (closeCheck) {
+                if (!closeCheck.get()) return map.get(key);
             }
         }
         return null;
@@ -48,9 +49,7 @@ public class ContainerMap {
      * Считаем, что метод вызывается только один раз
      */
     public void close() {
-        synchronized (ContainerMap.class) {
-            closeCheck.compareAndSet(false, true);
-            map.close();
-        }
+        closeCheck.compareAndSet(false, true);
+        map.close();
     }
 }
