5681c1a26bbaae77e633a2648acd9fab66acc225
Anna <anya.lakeeva1@mail.ru>
2020-04-25 16:38:32 +0300

    Синглтон, Мапа

--- src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
+++ src/main/java/ru/spsuace/projects/pass_fail/Singleton.java
@@ -1,17 +1,33 @@
 package ru.spsuace.projects.pass_fail;
 
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Надо реализовать ленивый синглтон. Важно, что бы объект можно было получить только через метод getInstance()
  * Так же важно, чтобы объект был создан только один раз и метод getInstance() всегда возвращал один и тот же объект.
- *
+ * <p>
  * getInstance может вызываться из многих потоков одновременно.
- *
+ * <p>
  * НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ AtomicReference
  */
 public class Singleton {
 
+    private static volatile Singleton INSTANCE;
+    private final int id;
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    public Singleton(int id) {
+        this.id = id;
+    }
+
     public static Singleton getInstance() {
-        return null;
+        if (INSTANCE == null) {
+            synchronized (Singleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new Singleton(counter.getAndIncrement());
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
--- src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
+++ src/main/java/ru/spsuace/projects/pass_fail/map/ContainerMap.java
@@ -3,38 +3,53 @@ package ru.spsuace.projects.pass_fail.map;
 
 /**
  * Реализовать контейнер для мапы. Сам класс ClosableMap считается потокобезопасным и с ним проблем нет. Его НЕ трогать.
- * Суть в том, что после вызова метода map.close() любой другой метод map будет кидать ошибкуи крашить данные.
+ * Суть в том, что после вызова метода map.close() любой другой метод map будет кидать ошибку крашить данные.
  * Надо избежать этого.
- *
+ * <p>
  * Можно создавать любые дополнительные методы и поля.
  * Так же можно использовать любые механизмы и объекты синхранизации в java.
- *
+ * <p>
  * Считаем, что метод close вызывается только один раз.
  * Все методы могут вызываться из разных потоков
  */
 public class ContainerMap {
 
     private ClosableMap map = new ClosableMap();
+    private Boolean Switch = false;
 
     /**
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long put(Long key, Long value) {
-        return map.put(key, value);
+        synchronized (this) {
+            if (!Switch) {
+                return map.put(key, value);
+            } else {
+                return null;
+            }
+        }
     }
-
 
     /**
      * Нельзя, чтобы вызывались методы map после вызова map.close(). В этом случае можно вернуть null
      */
     public Long get(Long key) {
-        return map.get(key);
+        synchronized (this) {
+            if (!Switch) {
+                return map.get(key);
+            } else {
+                return null;
+            }
+        }
     }
 
     /**
      * Считаем, что метод вызывается только один раз
      */
     public void close() {
-        map.close();
+        synchronized (this) {
+            Switch = true;
+            map.close();
+        }
     }
 }
